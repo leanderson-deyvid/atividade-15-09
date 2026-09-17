71e5ec1dc2230819d00dc4d54120bed63e8c6faa
leanderson-deyvid <leandersondeyvid1@gmail.com>
2026-09-17 16:28:52 -0300

    Add files via upload

---
+++ exercício/atividade 4.py
@@ -0,0 +1,17 @@
+nota = 5
+nota = 7
+nota = 8
+
+soma = 5 + 7 + 8
+media = soma/3
+
+print(media)
+
+
+if media>=7:
+    print("aprovado") 
+
+else: 
+    print("reprovado")
+
+      
---
+++ exercício/exercicio.py
@@ -0,0 +1,10 @@
+numero = int(input(10))
+antecessor = 10-1
+sucessor = 10+1
+dobro = 10*2
+triplo = 10*3
+
+print("numero",numero)
+print("antecessor",antecessor)
+print("dobro",dobro)
+print("triplo",triplo)
